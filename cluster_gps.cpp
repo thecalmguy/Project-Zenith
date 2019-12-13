9256e7b264e748ed624d1c8ac5f2c11ae83e74c0
otoshuki <guining.pertin.iitg@gmail.com>
2019-12-14 03:25:50 +0530

    Updated cluster_gps.cpp

--- cluster_gps.cpp
+++ cluster_gps.cpp
@@ -4,49 +4,59 @@
 
 #define RAND_MAX 100
 
+//Global variables
+float64 avg_val[2];
+
 //Function to cluster incoming GPS coordinates
-void cluster_gps(std::vector<float64> lat_data, std::vector<float64> lon_data)  {
+void cluster_gps(std::vector<std::pair <float64, float64>> gps_data)  {
   //Initialize radius of windows
   int rad = 0.5;
   ////////////////
   //Normalize data
   ////////////////
   //Use initial data points as centroid
-  std::vector<float64> lat_cent;
-  std::vector<float64> lon_cent;
+  std::vector<std::pair <float64, float64>> gps_cent;
   while(1)  {
     //New centroids
-    std::vector<float64> lat_new_cent;
-    std::vector<float64> lon_new_cent;
+    std::vector<std::pair <float64, float64>> gps_new_cent;
     //For each centroid
-    for(int cent_i=0; cent_i != lat_data.size(); cent_i++) {
+    for(int cent_i=0; cent_i != gps_data.size(); cent_i++) {
       //Data points within range
-      std::vector<float64> lat_within_range;
-      std::vector<float64> lon_within_range;
+      std::vector<std::pair <float64, float64>> gps_within_range;
       //Check distance of each data point
-      for(int data_i=0; data_i != lat_data.size(); data_i++)  {
+      for(int data_i=0; data_i != gps_data.size(); data_i++)  {
         //If within distance add it to "within range" list
-        if(get_distance(lat_cent[cent_i]-lat_data[data_i],lon_cent[cent_i]-lon_data[data_i])<rad) {
-          lat_within_range.push_back(lat_data[data_i]);
-          lon_within_range.push_back(lon_data[data_i]);
+        if(get_distance(gps_cent[cent_i].first-gps_data[data_i].first,gps_cent[cent_i].second-gps_data[data_i].second)<rad) {
+          gps_within_range.push_back(lon_data[data_i]);
         }
       }
       //New centroid is the mean of data within range
-      lat_new_cent.push_back(get_average(lat_within_range));
-      lon_new_cent.push_back(get_average(lon_within_range));
+      get_average(gps_within_range);
+      gps_new_cent.push_back(avg_val);
+      //To implement
+      //Get the unique centroids of gps_new_cent
+      //Check convergence
+      //If yes, set gps_cent=gps_new_cent and break
+      //If no, set gps_cent=gps_new_cent and run the loop
+
+      // std::vector<int>::iterator ip;
+      // ip = std::unique(gps_new_cent.begin(), gps_new_cent.end());
+      // // Resizing the vector so as to remove the undefined terms
+      // gps_new_cent.resize(std::distance(gps_new_cent.begin(), ip));
+
     }
     //Run until convergence
-    //Check overlaps and select required windows
   }
 }
 
 //Function to get average of a vector
-float64 get_average(std::vector<float64> inp) {
-  float64 avg = 0f;
+void get_average(std::vector<std::pair <float64, float64>> inp) {
   for (int i=0; i<inp.size(); i++)  {
-    avg += inp[i];
+    avg_val[0] += inp[i].first;
+    avg_val[1] += inp[i].second;
   }
-  return avg/inp.size()
+  avg_val[0] = avg_val[0]/inp.size();
+  avg_val[1] = avg_val[1]/inp.size();
 }
 
 //Function to get distance between points
@@ -56,7 +66,7 @@ float64 get_distance(float64 lat_dis, float64 lon_dist)  {
 
 //Function to check convergence
 bool check_convergence()  {
-  //Check number of data points within
+  //Check if gps_new_cent and gps_cent have the same elements
 }
 
 //Main function at least for now
