951159ac4e66b381488ee1ebb08c5c27b1a2c2c3
Manik Mittal <45773405+thecalmguy@users.noreply.github.com>
2019-12-14 03:45:33 +0530

    Added vector pair and gps offset for normalization

--- final_gps.cpp
+++ final_gps.cpp
@@ -7,9 +7,9 @@
 
 std_msgs::Int8 obj_found_1=0,obj_found_2=0,obj_found_3=0; 
 sensor_msgs::NavSatFix gps1, gps2, gps3;
+std::float64 lat_offset = 26.192, lon_offset=91.69;    // coordinates for guwahati
+std::vector<std::pair<int,int> > gps_coordinates;
 
-std::vector<float64> latitude;
-std::vector<float64> longitude;
 
 void obj_number_cb1(const std_msgs::Int8::ConstPtr& msg1)
 {
@@ -32,8 +32,7 @@ void gps_cb1(const sensor_msgs::NavSatFix::ConstPtr& msg4)
 	if (obj_found_1==1)
 	{
 		//append the vector with current gps coordinates
-		latitude.push_back(gps1.latitude);
-		longitude.push_back(gps1.longitude);
+		gps_coordinates.push_back(int((gps1.latitude-lat_offset)*10000000),int((gps1.longitude-lon_offset)*1000000));
 	}
 }
 
@@ -43,8 +42,7 @@ void gps_cb2(const sensor_msgs::NavSatFix::ConstPtr& msg5)
 	if (obj_found_2==1)
 	{
 		//append the vector with current gps coordinates
-		latitude.push_back(gps2.latitude);
-		longitude.push_back(gps2.longitude);
+		gps_coordinates.push_back(int((gps2.latitude-lat_offset)*10000000),int((gps2.longitude-lon_offset)*1000000));
 	}
 }
 
@@ -54,8 +52,7 @@ void gps_cb3(const sensor_msgs::NavSatFix::ConstPtr& msg6)
 	if (obj_found_3==1)
 	{
 		//append the vector with current gps coordinates
-		latitude.push_back(gps3.latitude);
-		longitude.push_back(gps3.longitude);
+		gps_coordinates.push_back(int((gps3.latitude-lat_offset)*10000000),int((gps3.longitude-lon_offset)*1000000));
 	}
 }
 
@@ -64,7 +61,7 @@ void gps_cb3(const sensor_msgs::NavSatFix::ConstPtr& msg6)
 
 int main(int argc, char **argv)
 {
-   ros::init(argc, argv, "gps_clustering");
+   ros::init(argc, argv, "gps_clustering_node");
    ros::NodeHandle nh;
    ros::Subscriber obj_sub1 = nh.subscribe<std_msgs::Int8>("/uav1/object_found", 100, obj_number_cb1);
    ros::Subscriber obj_sub2 = nh.subscribe<std_msgs::Int8>("/uav2/object_found", 100, obj_number_cb2);
