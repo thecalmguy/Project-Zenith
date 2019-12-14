1ef84ccab70975ec13f44e7b0771b9d277f2a0c5
otoshuki <guining.pertin.iitg@gmail.com>
2019-12-14 23:58:44 +0530

    Meanshift clustering using scikit learn

--- gps_clustering_node.py
+++ gps_clustering_node.py
@@ -7,6 +7,7 @@ from sklearn.cluster import MeanShift
 
 lat_offset = 26.192, lon_offset=91.69
 gps_coordinates=[]
+box_coordinates=[]
 
 obj_found_1=obj_found_2=obj_found_3 = Int8()
 
@@ -31,11 +32,21 @@ def gps_cb3(data):
     if obj_found_3==1:
         gps_coordinates.append((int((data.latitude-lat_offset)*10000000),int((data.longitude-lon_offset)*1000000)));
 
+def cluster_data(bandwidth):
+    #Define bandwidth for the clustering
+    global gps_coordinates
+    #Run only when gps_coordinates variable is not empty
+    if not len(gps_coordinates):
+        #Perform mean shift clustering
+        cluster_obj = MeanShift(bandwidth).fit(gps_coordinates)
+        #Print number of clusters for given bandwidth
+        print("No. of clusters detected: {0}".format(len(cluster_obj.cluster_centers_)))
+        #Update box coordinates
+        box_coordinates = cluster_obj.cluster_centers_
 
 #rospy.spin()
 
 def talker():
-    # pub = rospy.Publisher('gps_clustering_node', String, queue_size=10)
     rospy.init_node('gps_clustering_node', anonymous=True)
     rospy.Subscriber("/uav1/mavros/global_position/global", NavSatFix, gps_cb1)
     rospy.Subscriber("/uav2/mavros/global_position/global", NavSatFix, gps_cb2)
@@ -43,9 +54,9 @@ def talker():
     rospy.Subscriber("/uav1/obj_found", Int8, obj_number_cb1)
     rospy.Subscriber("/uav2/obj_found", Int8, obj_number_cb2)
     rospy.Subscriber("/uav3/obj_found", Int8, obj_number_cb3)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-        
+        cluster_data(50)
         # rospy.loginfo(hello_str)
         # pub.publish(hello_str)
         rate.sleep()
@@ -55,4 +66,3 @@ if __name__ == '__main__':
         talker()
     except rospy.ROSInterruptException:
         pass
-
