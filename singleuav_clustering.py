808671765ed3ada6dc48e36f76ae43e65dbe379a
Manik Mittal <45773405+thecalmguy@users.noreply.github.com>
2019-12-15 19:03:28 +0530

    updated after debugging

--- singleuav_clustering.py
+++ singleuav_clustering.py
@@ -5,25 +5,33 @@ from sensor_msgs.msg import NavSatFix
 from sklearn.cluster import MeanShift
 #from threading import Thread
 
-lat_offset = 26.19, lon_offset=91.69
+lat_offset = 26.19
+lon_offset=91.69
 gps_coordinates=[]
 box_coordinates=[]
 box_gps_msg = NavSatFix()
 
-obj_found_1=obj_found_2=obj_found_3 = Int8()
+obj_found_1=56
 
-def obj_number_cb1(data):
-    obj_found_1=data
+def obj_number_cb1(data_temp):
+    print("obj data updated")
+    global obj_found_1 
+    obj_found_1 = int(data_temp.data)
+    print(obj_found_1)
 
 def gps_cb1(data):
-    if obj_found_1==1:
+    global obj_found_1, lat_offset, lon_offset
+    print("gps called")
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ",obj_found_1)
+    if (obj_found_1==1) :
         print("*****************object found ******************")
         gps_coordinates.append((int((data.latitude-lat_offset)*10000000),int((data.longitude-lon_offset)*1000000)));
 
 def cluster_data(bandwidth):
     #Define bandwidth for the clustering
     print("cluster code called")
-    global gps_coordinates
+    print(len(gps_coordinates))
+    global gps_coordinates, box_coordinates
     #Run only when gps_coordinates variable is not empty
     if len(gps_coordinates):
         #Perform mean shift clustering
@@ -34,10 +42,7 @@ def cluster_data(bandwidth):
         print("No. of clusters detected: {0}".format(len(cluster_obj.cluster_centers_)))
         #Update box coordinates
         box_coordinates = cluster_obj.cluster_centers_
-        for box in box_coordinates:
-            box_gps_msg.latitude = (box[0]/10000000)+lat_offset
-            box_gps_msg.longitude = (box[1]/1000000)+lon_offset
-            pub.publish(box_gps_msg)
+        
 
 #rospy.spin()
 
@@ -46,8 +51,14 @@ def cluster_node_func():
     rospy.Subscriber("/mavros/global_position/global", NavSatFix, gps_cb1)
     rospy.Subscriber("/obj_found", Int8, obj_number_cb1)
     pub = rospy.Publisher('/box_gps', NavSatFix, queue_size=10)
+    rate = rospy.Rate(10)
+    global box_coordinates
     while not rospy.is_shutdown():
         cluster_data(50)
+        for box in box_coordinates:
+            box_gps_msg.latitude = (box[0]/10000000)+lat_offset
+            box_gps_msg.longitude = (box[1]/1000000)+lon_offset
+            pub.publish(box_gps_msg)
         
         rate.sleep()
 
